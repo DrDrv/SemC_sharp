208f5ae52887a81455d084945718165695e093dd
Drv <dzen.driver@gmail.com>
2022-02-24 23:44:23 +0300

    Zadacha 4 ready

---
+++ Zad4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Massive
+{// Вход три числа - Выход: Максимальное
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите три числа:");
+            Console.Write("Первое число: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Второе число: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Третье число: ");
+            double d = Convert.ToDouble(Console.ReadLine());
+            double max = a;
+            if (max < b) max = b;
+            if (max < d) max = d;
+            Console.WriteLine("Максимальное число из введенных: " + max);
+        }
+    }
+}
