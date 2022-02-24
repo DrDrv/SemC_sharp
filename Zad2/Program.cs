c51cd72d71055d4fcfb193f155b7a9456517f29e
Drv <dzen.driver@gmail.com>
2022-02-24 23:19:07 +0300

    Zadacha 2 ready

---
+++ Zad2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Massive
+{// Вход два числа - Выход: Большее и Меньшее
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите два числа:");
+            Console.Write("Первое число: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Второе число: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            if (a < b)
+            {
+                Console.WriteLine(a + " - Меньшее число, " + b + " - Большее число, ");
+            }
+            else if (a > b)
+            {
+                Console.WriteLine(b + " - Меньшее число, " + a + " - Большее число, ");
+            }
+            else Console.WriteLine("Числа равны!");
+        }
+    }
+}
