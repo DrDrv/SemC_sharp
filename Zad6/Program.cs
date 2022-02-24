ba952ca094a3763b984c25dee6d95910bab18f32
Drv <dzen.driver@gmail.com>
2022-02-24 23:55:00 +0300

    Zadacha 6 ready

---
+++ Zad6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Massive
+{// Вход число - Выход: четное!?
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите число: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            if (a % 2 == 0) 
+            {
+                Console.WriteLine("Число является ЧЕТНЫМ");
+            }
+            else Console.WriteLine("Число НЕ является ЧЕТНЫМ");
+        }
+    }
+}
