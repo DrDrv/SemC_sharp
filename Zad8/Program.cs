9d4a2451ea2b17500f711df0433bdf9533008b27
Drv <dzen.driver@gmail.com>
2022-02-25 00:09:36 +0300

    Zadacha 8 ready

---
+++ Zad8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Massive
+{// Вход число - Выход: четные целые числа от 1 до числа!?
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите число: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            if (a > 1 ) 
+            {
+                Console.Write("Числовая последовательность четных чисел: ");
+                for (int i = 2; i<=a; i+=2) Console.Write(i + ", ");
+            }
+            else Console.WriteLine("Числовую последовательность построить НЕ удалось!");
+        }
+    }
+}
