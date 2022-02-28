41eb2d5be45d13bb5068cc5c751fd126b17a4569
Drv <dzen.driver@gmail.com>
2022-03-01 00:16:27 +0300

    Задача 10 ready

--- Zad10/Program.cs
+++ Zad10/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вход многозначное число - Выход перевертыш
+Console.WriteLine("Разворачиваем числа");
+Random rand = new Random();
+for (int i = 1; i <= 3; i++)
+{
+    int number = rand.Next(100, 1000);
+    Console.WriteLine("Исходное число - " + number + " ----> " + Metod(number));
+}
+// Метод универсальный - можно разворачивать любые целые положительные числа =))) 
+int Metod(int s)
+{
+    int i = 0;
+    while (s >= 1)
+    {
+        i *= 10;
+        i = i + s%10;
+        s = s/10;
+    }
+    return (i);
+}
