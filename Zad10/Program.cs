69dcee247b29b2b1a86fe838a048ccec3048e78a
DrDrv <dzen.driver@gmail.com>
2022-03-01 15:52:02 +0300

    Zadacha 10 & 13 Ready

--- Zad10/Program.cs
+++ Zad10/Program.cs
@@ -1,20 +1,43 @@
 ﻿// Вход многозначное число - Выход перевертыш
-Console.WriteLine("Разворачиваем числа");
+Console.WriteLine("Разворачиваем числа и находим третье число числовой последовательности");
 Random rand = new Random();
-for (int i = 1; i <= 3; i++)
+for (int i = 1; i <= 6; i++)
 {
-    int number = rand.Next(100, 1000);
-    Console.WriteLine("Исходное число - " + number + " ----> " + Metod(number));
+    int number = rand.Next(10, 100000);
+    Console.WriteLine("Исходное число - " + number + " --Математически--> " + Mmetod(number));
+    Console.WriteLine("Исходное число - " + number + " --   Текстом   --> " + Smetod(Convert.ToString(number),0));
+    Console.WriteLine("Третье число числовой последовательности : " + Smetod(Convert.ToString(number),1));
+    Console.WriteLine("----------------------------");
 }
-// Метод универсальный - можно разворачивать любые целые положительные числа =))) 
-int Metod(int s)
+// Метод математический универсальный - можно разворачивать любые целые положительные числа =)))
+// если число имеет нули в конце числа, то разрядность числа математически уменьшается, т.к. в начале числа\
+// 0 не указывается 
+int Mmetod(int n)
 {
     int i = 0;
-    while (s >= 1)
+    while (n >= 1)
     {
         i *= 10;
-        i = i + s%10;
-        s = s/10;
+        i = i + n%10;
+        n = n/10;
     }
     return (i);
 }
+// Текстовый метод переворота универсальный - можно разворачивать любое количество символов из которых состоит
+// число
+string Smetod(string s, int d)
+{
+    char[] arr = s.ToCharArray();
+    string t = "";
+    for (int k=(s.Length-1); k>=0; k--)
+    {
+        t = t+arr[k];
+    }
+    if (d==0) 
+    {return (t);}
+    else if (d==1) // Выводим третье число из числовой последовательности
+    {   return (Convert.ToString(arr[2]));}
+    else return("Error");
+}
+
+
