386f29a680d08dcf1308acfcdea88e727ebb3f03
DrDrv <dzen.driver@gmail.com>
2022-03-03 14:46:34 +0300

    Zadacha 3 in 1 Ready. Fix Smetod

--- Zad10/Program.cs
+++ Zad10/Program.cs
@@ -6,9 +6,9 @@ for (int i = 1; i <= 6; i++)
 {
     int number = rand.Next(1, 1000000);
     Console.WriteLine("Исходное число - " + number + " --Математически--> " + Mmetod(number));
-    Console.WriteLine("Исходное число - " + number + " --   Текстом   --> " + Smetod(Convert.ToString(number),0));
-    Console.WriteLine("Третье число числовой последовательности : " + Smetod(Convert.ToString(number),1));
-    int j = Convert.ToInt32(Smetod(Convert.ToString(number),1));
+    Console.WriteLine("Исходное число - " + number + " --   Текстом   --> " + Smetod(Convert.ToString(number), 0));
+    Console.WriteLine("Третье число числовой последовательности : " + Smetod(Convert.ToString(number), 1));
+    int j = Convert.ToInt32(Smetod(Convert.ToString(number), 1));
     Console.WriteLine("День недели - " + j + "     " + Dmetod(j));
     Console.WriteLine("----------------------------");
 }
@@ -21,8 +21,8 @@ int Mmetod(int n)
     while (n >= 1)
     {
         i *= 10;
-        i = i + n%10;
-        n = n/10;
+        i = i + n % 10;
+        n = n / 10;
     }
     return (i);
 }
@@ -31,25 +31,27 @@ int Mmetod(int n)
 string Smetod(string s, int d)
 {
     char[] arr = s.ToCharArray();
-    string t = "";
-    for (int k=(s.Length-1); k>=0; k--)
+    if (d == 0) //Вывод разворота текста числовой последовательности
     {
-        t = t+arr[k];
+        string t = "";
+        for (int k = (s.Length - 1); k >= 0; k--)
+        {
+            t = t + arr[k];
+        }
+        return (t);
     }
-    if (d==0) 
-    {return (t);}
-    else if (d==1) // Выводим третье число из числовой последовательности
-    {   
-        if (s.Length<3) {return("Третьей цифры нет");} 
-        else {return (Convert.ToString(arr[2]));}
+    else if (d == 1) // Выводим третье число из числовой последовательности
+    {
+        if (s.Length < 3) { return ("Третьей цифры нет"); }
+        else { return (Convert.ToString(arr[2])); }
     }
-    else return("Error");
+    else return ("Error");
 }
 //Функция определения соответствия дня недели и определения выходной ли день
 string Dmetod(int v)
 {
-    if ((v < 1) | (v > 7)) v=0;
-    string[] darr = {"Этот день недели вне времени","Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"};
-    if (v>5) darr[v] += " - Выходной!";
+    if ((v < 1) | (v > 7)) v = 0;
+    string[] darr = { "Этот день недели вне времени", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+    if (v > 5) darr[v] += " - Выходной!";
     return (darr[v]);
 }
