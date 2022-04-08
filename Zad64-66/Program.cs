388ee5838c5559ce60240c93cc8d01e88d10c907
DrDrv <dzen.driver@gmail.com>
2022-04-08 12:00:35 +0300

    Zad64-66 Ready

---
+++ Zad64-66/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа кратные 3-ём 
+//в промежутке от M до N.
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+//в промежутке от M до N.
+Random rnd = new Random();
+int numberM = rnd.Next(1,10);
+int numberN = rnd.Next(10,50);
+int summelements = 0;
+// Решение задачи 64
+Console.WriteLine($"Нахождение чисел кратных 3 от {numberM} до {numberN}:");
+NatNumDevThree(numberM);
+Console.WriteLine("");
+// Коенц задачи 64
+Console.WriteLine("");
+// Решение задачи 66
+Console.WriteLine($"Cумма натуральных элементов в промежутке {numberM} до {numberN}: {summelements}");
+// Конец задачи 66
+void NatNumDevThree(int numM)
+{
+    summelements += numM;
+    if (numM%3 == 0) Console.Write($"{numM} \t");
+    if (numM == numberN) return;
+    NatNumDevThree(numM+1); 
+    return;
+}
