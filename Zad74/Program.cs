6d448c5dbee9c39f88c666b3e5ef99b3d6d3d989
Drv <dzen.driver@gmail.com>
2022-04-16 14:51:19 +0300

    Zad2 Ready

---
+++ Zad74/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int numberN = 10;
+int j = 1;
+int A = 0, B = 0;
+
+ for(int i=1; i<=numberN; i+=j) 
+ {
+    if (i < numberN)
+    {
+        A = i;
+        B += i-j; 
+    } 
+   j=i-j;
+  }
+Console.WriteLine(A);
+Console.WriteLine(B+1);
--- ZadS/Program.cs
+++ ZadS/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 class Programm
 {
-    static void Main()
+    static void Main(string[] args)
     {
         Random rnd = new Random();
         Console.WriteLine($"+= Решение задачи 1 =+");
@@ -21,6 +21,29 @@ class Programm
             rezult += num%10;
             Perevert (num/10);
             return(0);
+        }
+
+        Console.WriteLine($"+= Решение задачи 2 =+");
+        //2. Дана монотонная последовательность, в которой каждое натуральное число n встречается ровно n раз: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, ... 
+        //Дано число m. Выведите первые m членов этой последовательности.
+        //m = 5 >> 1, 2, 2, 3, 3
+        numberA = rnd.Next(5,15);
+        Console.WriteLine($"Входное число - {numberA}");
+        PrintList(numberA);
+        Console.WriteLine();
+        
+        void PrintList(int num, int nownumber = 1, int kolvo = 1)
+        {
+            if (num == 0) return;
+            Console.Write($"{nownumber} \t");
+            if (nownumber == kolvo)
+            {
+                kolvo = 0;
+                nownumber++;
+            } 
+            kolvo++;
+            PrintList(num-1,nownumber,kolvo);
+            return;
         }
 
     }
